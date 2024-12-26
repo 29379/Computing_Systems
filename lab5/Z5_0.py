4348f4d187b7f8226ef146e42dce59d80f9ac728
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2024-12-27 00:09:58 +0100

    lab5 done

---
+++ lab5/Z5_0.py
@@ -0,0 +1,13 @@
+# import tensorflow as tf
+import os
+# import keras
+
+
+def main():
+    # os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+    print("\n - - -")   
+
+
+
+if "name" == "__main__":
+    main()
